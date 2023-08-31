8ddb00e20e72daf6f0470031fff57c9f7e12a1e8
Mateusz Cholewka <mateusz@cholewka.com.pl>
2023-08-31 19:37:31 +0200

    Prepare initial Applicaiton class

---
+++ src/PhativeApplication.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phative\Framework;
+
+use Phative\Render\Renderer;
+use Phative\Render\Widget\Factory\FrameWidgetFactory;
+use Tkui\DotEnv;
+use Tkui\TclTk\TkAppFactory;
+use Tkui\TclTk\TkApplication;
+use Tkui\Windows\MainWindow;
+
+abstract class PhativeApplication
+{
+    private TkApplication $app;
+    private MainWindow $window;
+
+    public function __construct(string $appName,)
+    {
+        $factory = new TkAppFactory($appName);
+        $this->app = $factory->createFromEnvironment(DotEnv::create(dirname(__DIR__)));
+        $this->window = new MainWindow($this->app, $appName);
+    }
+
+    public function run(): void
+    {
+        $layout = $this->render();
+
+        $rendered = (new Renderer())->render($this->window, $layout);
+
+        $this->window->pack($rendered);
+
+        $this->app->run();
+    }
+
+    abstract public function render(): FrameWidgetFactory;
+}
