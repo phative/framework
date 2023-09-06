79f85ed6e31dc75245057481f164f2eb6374e6f9
Mateusz Cholewka <mateusz@cholewka.com.pl>
2023-09-07 00:29:23 +0200

    Add theme management

--- src/PhativeApplication.php
+++ src/PhativeApplication.php
@@ -13,23 +13,33 @@ use Tkui\Windows\MainWindow;
 
 abstract class PhativeApplication
 {
-    private TkApplication $app;
-    private MainWindow $window;
+    protected TkApplication $app;
+    protected MainWindow $window;
 
-    public function __construct(string $appName,)
+    public function __construct(string $appName)
     {
         $factory = new TkAppFactory($appName);
         $this->app = $factory->createFromEnvironment(DotEnv::create(dirname(__DIR__)));
         $this->window = new MainWindow($this->app, $appName);
     }
 
+    public function loadTheme(string $path): void
+    {
+        $this->app->tclEval('source', $path);
+    }
+
+    public function setTheme(string $name): void
+    {
+        $this->app->tclEval('set_theme', $name);
+    }
+
     public function run(): void
     {
         $layout = $this->render();
 
-        $rendered = (new Renderer())->render($this->window, $layout);
+        [$rendered, $styles] = (new Renderer())->render($this->window, $layout);
 
-        $this->window->pack($rendered);
+        $this->window->pack($rendered, $styles);
 
         $this->app->run();
     }
